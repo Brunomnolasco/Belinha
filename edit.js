acf09f9fdc43e5beaae3998cb2bf995e40e85be1
Bruno Nolasco <bruno2403moreira@gmail.com>
2022-04-21 15:14:37 -0300

    Projeto Belinha

---
+++ edit.js
@@ -0,0 +1,7 @@
+function abrirMenu()
+{
+    const menu = document.querySelector('.menu')
+    const navegacao = document.querySelector('.navegacao')
+    menu.classList.toggle('active')
+    navegacao.classList.toggle('active')
+}
